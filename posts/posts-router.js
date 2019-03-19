cdec194c437fe24be8a28327fd5eac7b1847e974
ajb85 <ajbrush85@gmail.com>
2019-03-19 16:07:41 -0400

    api/posts/:id delete request added

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -60,7 +60,21 @@ router.get("/:id", (req, res) => {
         .json({ error: "The post information could not be retrieved." })
     );
 });
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  db.remove(req.params.id)
+    .then(success => {
+      success
+        ? res.status(200).end()
+        : res
+            .status(404)
+            .json({
+              message: "The post with the specified ID does not exist."
+            });
+    })
+    .catch(err =>
+      res.status(500).json({ error: "The post could not be removed" })
+    );
+});
 router.put("/:id", (req, res) => {});
 
 module.exports = router;
